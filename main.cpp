0ca24d868421399da42a9eec1895be592229374b
unknown <Nils@.(none)>
2010-05-13 22:40:28 +0200

    .

--- main.cpp
+++ main.cpp
@@ -23,25 +23,28 @@ int main(void)
 
 
 	int x,y;
+    unsigned char r,g,b;
+    int r255, g255, b255;
+    short color;
+
 	for(x = 0; x < 180; x+=1)
 	{
 		for(y = 0; y < 220; y+=1)
 		{
 		    RGBApixel rgba = bmp->GetPixel(x, y);
 
-            int r255 = (int)(rgba.Red), g255 = (int)(rgba.Green), b255 = (int)(rgba.Blue);
-
-            unsigned char r,g,b;
-
+            r255 = (int)(rgba.Red);
+            g255 = (int)(rgba.Green);
+            b255 = (int)(rgba.Blue);
 
             //printf("R: %d, G: %d, B: %d\n", r, g, b);
             //printf("R: %d, G: %d, B: %d\n", r255, g255, b255);
 
-            /*r = (int)(((double)(r255))/255.0*31.0);
-            g = (int)(((double)(g255))/255.0*63.0);
-            b = (int)(((double)(b255))/255.0*31.0);
-*/
-            short color = 0;
+            r = (int)(floor(((double)(r255))/255.0*31.0));
+            g = (int)(floor(((double)(g255))/255.0*63.0));
+            b = (int)(floor(((double)(b255))/255.0*31.0));
+
+            color = 0;
 
             r = rgba.Red;
             g = rgba.Green;
@@ -51,7 +54,7 @@ int main(void)
             color |= g << 6;
             color |= r << 11;
 
-			fb[240 * y + x] = color;
+			fb[180 * y + x] = color;
 		}
 	}
 
